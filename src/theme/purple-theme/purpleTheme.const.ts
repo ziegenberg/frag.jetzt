60183be870059c49ed094343c339426ba7d4d132
Lukas Haase <lukas.haase@mni.thm.de>
2019-09-05 15:26:23 +0200

    ProfessorTells, Prev_Frame_Arrow to Arrow_Back

--- src/theme/purple-theme/purpleTheme.const.ts
+++ src/theme/purple-theme/purpleTheme.const.ts
@@ -24,32 +24,4 @@ export const purple = {
   '--grey': 'grey',
   '--grey-light': 'lightgrey',
   '--black': 'black'
-
-  /*
-  '--primary' : '#4a8270',
-  '--primary-variant': '#4a8270',
-
-  '--secondary': '#4a8270',
-  '--secondary-variant': '#4a8270',
-
-  '--background': '#c8c4aa',
-  '--surface': '#e0dcbf',
-  '--dialog': '#e0dcbf',
-
-  '--on-primary': '#e0dcbf',
-  '--on-secondary': '#e0dcbf',
-  '--on-background': '#000000',
-  '--on-surface': '#000000',
-
-  '--green': '#4caf50',
-  '--red': '#f44336',
-  '--yellow': '#FFD54F',
-  '--blue': '#3f51b5',
-  '--purple': '#9c27b0',
-  '--light-green': '#80ba24',
-  '--grey': '#BDBDBD',
-  '--grey-light': '#EEEEEE',
-  '--black': '#212121'
-
-   */
 };
