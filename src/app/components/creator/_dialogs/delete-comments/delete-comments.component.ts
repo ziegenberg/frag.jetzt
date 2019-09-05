876ccf142f47e182d31e5631338cd1229a3f56a5
Jens Hansen <jens.hansen@mni.thm.de>
2019-09-05 15:53:27 +0200

    Migrate delete comments component to use generic dialog actions component

--- src/app/components/creator/_dialogs/delete-comments/delete-comments.component.ts
+++ src/app/components/creator/_dialogs/delete-comments/delete-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { RoomEditComponent } from '../room-edit/room-edit.component';
+import { DialogConfirmActionButtonType } from '../../../shared/dialog/dialog-action-buttons/dialog-action-buttons.component';
 
 @Component({
   selector: 'app-delete-comment',
@@ -9,14 +10,31 @@ import { RoomEditComponent } from '../room-edit/room-edit.component';
 })
 export class DeleteCommentsComponent implements OnInit {
 
+  /**
+   * The confirm button type of the dialog.
+   */
+  confirmButtonType: DialogConfirmActionButtonType = DialogConfirmActionButtonType.Alert;
+
+
   constructor(public dialogRef: MatDialogRef<RoomEditComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
   }
 
-  close(type: string): void {
-    this.dialogRef.close(type);
+
+  /**
+   * Returns a lambda which closes the dialog on call.
+   */
+  buildCloseDialogActionCallback(): () => void {
+    return () => this.dialogRef.close('abort');
   }
 
+
+  /**
+   * Returns a lambda which executes the dialog dedicated action on call.
+   */
+  buildCommentsDeleteActionCallback(): () => void {
+    return () => this.dialogRef.close('delete');
+  }
 }
