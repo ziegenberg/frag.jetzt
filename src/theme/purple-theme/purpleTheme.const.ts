d72c8c3e06bca446178ed076571c1f6d59c47bd4
Lukas Haase <lukas.haase@mni.thm.de>
2019-09-05 13:06:20 +0200

    Update2

--- src/theme/purple-theme/purpleTheme.const.ts
+++ src/theme/purple-theme/purpleTheme.const.ts
@@ -1,12 +1,38 @@
 export const purple = {
 
+  '--primary' : 'Maroon',
+  '--primary-variant': 'Moccasin',
+
+  '--secondary': 'black',
+  '--secondary-variant': 'green',
+
+  '--background': 'DarkKhaki',
+  '--surface': 'Moccasin',
+  '--dialog': '#f2f4f5',
+
+  '--on-primary': '#FFFFFF',
+  '--on-secondary': '#ffffff',
+  '--on-background': '#000000',
+  '--on-surface': '#000000',
+
+  '--green': 'green',
+  '--red': 'red',
+  '--yellow': 'yellow',
+  '--blue': 'blue',
+  '--purple': 'purple',
+  '--light-green': 'lightgreen',
+  '--grey': 'grey',
+  '--grey-light': 'lightgrey',
+  '--black': 'black'
+
+  /*
   '--primary' : '#4a8270',
   '--primary-variant': '#4a8270',
 
   '--secondary': '#4a8270',
   '--secondary-variant': '#4a8270',
 
-  '--background': '#d9d5b8',
+  '--background': '#c8c4aa',
   '--surface': '#e0dcbf',
   '--dialog': '#e0dcbf',
 
@@ -24,4 +50,6 @@ export const purple = {
   '--grey': '#BDBDBD',
   '--grey-light': '#EEEEEE',
   '--black': '#212121'
+
+   */
 };
