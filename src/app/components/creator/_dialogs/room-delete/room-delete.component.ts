cbbb013a645ac8b12adafc02080f2c6385046c6e
Jens Hansen <jens.hansen@mni.thm.de>
2019-09-05 14:38:28 +0200

    Migrate room delete component to use generic dialog actions component

--- src/app/components/creator/_dialogs/room-delete/room-delete.component.ts
+++ src/app/components/creator/_dialogs/room-delete/room-delete.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { Room } from '../../../../models/room';
 import { RoomEditComponent } from '../room-edit/room-edit.component';
+import { DialogConfirmActionButtonType } from '../../../shared/dialog/dialog-action-buttons/dialog-action-buttons.component';
 
 @Component({
   selector: 'app-room-delete',
@@ -11,14 +12,41 @@ import { RoomEditComponent } from '../room-edit/room-edit.component';
 export class RoomDeleteComponent implements OnInit {
   room: Room;
 
+
+  /**
+   * The confirm button type of the delete account.
+   */
+  confirmButtonType: DialogConfirmActionButtonType = DialogConfirmActionButtonType.Alert;
+
+
   constructor(public dialogRef: MatDialogRef<RoomEditComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
-  onNoClick(): void {
+  ngOnInit() {
+  }
+
+
+  /**
+   * Closes the dialog on call.
+   */
+  closeDialog(): void {
     this.dialogRef.close();
   }
 
-  ngOnInit() {
+
+  /**
+   * Returns a lambda which closes the dialog on call.
+   */
+  buildCloseDialogActionCallback(): () => void {
+    return () => this.closeDialog();
+  }
+
+
+  /**
+   * Returns a lambda which executes the dialog dedicated action on call.
+   */
+  buildRoomDeleteActionCallback(): () => void {
+    return () => this.dialogRef.close('delete');
   }
 }
