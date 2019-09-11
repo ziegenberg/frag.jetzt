bde2c1ddc84255767e8aa9f6778c3988ce9d90bc
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-11 14:41:59 +0200

    Add hyphenation, separate text into paragraphs, fix gap issue between text and video

--- src/assets/i18n/demo/demo-de.ts
+++ src/assets/i18n/demo/demo-de.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-demo-de',
-  templateUrl: './demo-de.html'
+  templateUrl: './demo-de.html',
+  styleUrls: ['./demo.scss']
 })
 
 export class DemoDeComponent {
--- src/assets/i18n/demo/demo-en.ts
+++ src/assets/i18n/demo/demo-en.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-demo-en',
-  templateUrl: './demo-en.html'
+  templateUrl: './demo-en.html',
+  styleUrls: ['./demo.scss']
 })
 
 export class DemoEnComponent {
