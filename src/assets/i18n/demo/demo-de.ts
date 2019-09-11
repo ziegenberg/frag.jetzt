a7f237f091caa02f649be29f3feaa72654e2cce1
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-11 10:57:15 +0200

    Add introduction text above demo video.

---
+++ src/assets/i18n/demo/demo-de.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-de',
+  templateUrl: './demo-de.html'
+})
+
+export class DemoDeComponent {
+}
---
+++ src/assets/i18n/demo/demo-en.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-en',
+  templateUrl: './demo-en.html'
+})
+
+export class DemoEnComponent {
+}
